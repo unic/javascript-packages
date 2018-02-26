437562800f4eebf464e4d68903453814f53bc7c2
christiansany <christian.sany@bluewin.ch>
2018-02-26 13:54:26 +0100

    Refactor webpack & testrunning to repo based configuration

--- packages/composite-logger/webpack.config.js
+++ packages/composite-logger/webpack.config.js
@@ -1,34 +1,13 @@
-const webpack = require('webpack');
 const path = require('path');
+const baseConfig = require('../../webpack.config');
 
 const config = {
   entry: './src/index.js',
   output: {
-    library: 'logger',
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist'),
     filename: 'logger.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-      comments: false,
-    }),
-    new webpack.LoaderOptionsPlugin({
-      debug: true,
-    }),
-  ],
 };
 
-module.exports = config;
+module.exports = Object.assign({}, baseConfig, config);
