2ebbf79f2cb6566f4b139d189aab6e8e9c1c7161
Christian Sany <christian.sany@bluewin.ch>
2018-03-07 10:23:09 +0100

    Add a few tests to the pubsub singleton

--- packages/pubsub/src/index.test.js
+++ packages/pubsub/src/index.test.js
@@ -1,6 +1,17 @@
 import PubSub from './index';
 
-test('getInstance() always points to the same instance', () => {
+test('PubSub.getInstance() always points to the same instance', () => {
   const instance = PubSub.getInstance();
   expect(instance).toBe(PubSub.getInstance());
+});
+
+test('PubSub.getInstance() exposes an object', () => {
+  const instance = PubSub.getInstance();
+  expect(typeof instance).toBe('object');
+});
+
+test('The right methods are exposed', () => {
+  expect(['on', 'off', 'trigger']).toEqual(
+    expect.arrayContaining(Object.keys(PubSub.getInstance())),
+  );
 });
